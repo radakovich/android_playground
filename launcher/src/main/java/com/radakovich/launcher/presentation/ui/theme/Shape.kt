d9d3c2b62c1a6e3a05ab9ce007d12b6dedba8034
Jake Radakovich <jradakovich@eventbrite.com>
2022-11-15 16:28:14 -0600

    Added data, domain, and presentation layer to launcher module

--- launcher/src/main/java/com/radakovich/launcher/presentation/ui/theme/Shape.kt
+++ launcher/src/main/java/com/radakovich/launcher/presentation/ui/theme/Shape.kt
@@ -1,4 +1,4 @@
-package com.radakovich.launcher.ui.theme
+package com.radakovich.launcher.presentation.ui.theme
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Shapes
